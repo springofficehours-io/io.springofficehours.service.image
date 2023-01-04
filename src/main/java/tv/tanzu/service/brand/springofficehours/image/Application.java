473aebe9b834796754aee4b87e16211cb72317f7
dashaun <dashaun@dashaun.com>
2023-01-04 10:58:47 -0600

    rename Application

---
+++ src/main/java/tv/tanzu/service/brand/springofficehours/image/Application.java
@@ -0,0 +1,13 @@
+package tv.tanzu.service.brand.springofficehours.image;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
