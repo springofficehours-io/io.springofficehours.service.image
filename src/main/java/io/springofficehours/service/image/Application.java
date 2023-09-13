0eaae1ec39d597bf3f917388e8d9fb7e3c52e6db
DaShaun <dashaun@dashaun.com>
2023-09-13 11:04:29 -0700

    messing around

--- src/main/java/io/springofficehours/service/image/Application.java
+++ src/main/java/io/springofficehours/service/image/Application.java
@@ -2,6 +2,9 @@ package io.springofficehours.service.image;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +13,20 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+}
+
+@Configuration
+class RequestLoggingFilterConfig {
+
+	@Bean
+	public CommonsRequestLoggingFilter logFilter() {
+		CommonsRequestLoggingFilter filter
+				= new CommonsRequestLoggingFilter();
+		filter.setIncludeQueryString(true);
+		filter.setIncludePayload(true);
+		filter.setMaxPayloadLength(10000);
+		filter.setIncludeHeaders(false);
+		filter.setAfterMessagePrefix("REQUEST DATA: ");
+		return filter;
+	}
 }
