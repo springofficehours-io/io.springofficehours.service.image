f87c7cbcf5455fc4321af38c8182a4fbb9164104
dashaun <dashaun@dashaun.com>
2023-10-31 11:38:42 -0500

    3.1.5

--- src/main/java/io/springofficehours/service/image/Application.java
+++ src/main/java/io/springofficehours/service/image/Application.java
@@ -20,8 +20,7 @@ class RequestLoggingFilterConfig {
 
 	@Bean
 	public CommonsRequestLoggingFilter logFilter() {
-		CommonsRequestLoggingFilter filter
-				= new CommonsRequestLoggingFilter();
+		CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
 		filter.setIncludeQueryString(true);
 		filter.setIncludePayload(true);
 		filter.setMaxPayloadLength(10000);
@@ -29,4 +28,5 @@ class RequestLoggingFilterConfig {
 		filter.setAfterMessagePrefix("REQUEST DATA: ");
 		return filter;
 	}
+
 }
--- src/test/java/io/springofficehours/service/image/TestApplication.java
+++ src/test/java/io/springofficehours/service/image/TestApplication.java
@@ -1,7 +1,6 @@
 package io.springofficehours.service.image;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.devtools.restart.RestartScope;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.DynamicPropertyRegistry;
@@ -11,34 +10,28 @@ import org.testcontainers.containers.GenericContainer;
 @TestConfiguration(proxyBeanMethods = false)
 public class TestApplication {
 
-    @Bean
-    public GenericContainer lokiContainer(DynamicPropertyRegistry registry) {
-        GenericContainer container = new GenericContainer("grafana/loki")
-                .withExposedPorts(3100);
-        return container;
-    }
+	@Bean
+	public GenericContainer lokiContainer(DynamicPropertyRegistry registry) {
+		GenericContainer container = new GenericContainer("grafana/loki").withExposedPorts(3100);
+		return container;
+	}
 
-    @Bean
-    public GenericContainer grafanaContainer(DynamicPropertyRegistry registry) {
-        GenericContainer container = new GenericContainer("grafana/grafana")
-                .withClasspathResourceMapping("grafana-datasources.yaml",
-                        "/etc/grafana/provisioning/datasources/datasource.yml", 
-                        BindMode.READ_ONLY)
-                .withClasspathResourceMapping("grafana-dashboards.yaml",
-                        "/etc/grafana/provisioning/dashboards/dashboard.yml", 
-                        BindMode.READ_ONLY)
-                .withClasspathResourceMapping("microservices-spring-boot-2-1_rev1.json",
-                        "/etc/grafana/provisioning/dashboards/microservices-spring-boot-2-1_rev1.json",
-                        BindMode.READ_ONLY)
-                .withClasspathResourceMapping("grafana.ini",
-                        "/etc/grafana/grafana.ini",
-                        BindMode.READ_ONLY)
-                .withExposedPorts(3000);
-        return container;
-    }
+	@Bean
+	public GenericContainer grafanaContainer(DynamicPropertyRegistry registry) {
+		GenericContainer container = new GenericContainer("grafana/grafana")
+			.withClasspathResourceMapping("grafana-datasources.yaml",
+					"/etc/grafana/provisioning/datasources/datasource.yml", BindMode.READ_ONLY)
+			.withClasspathResourceMapping("grafana-dashboards.yaml",
+					"/etc/grafana/provisioning/dashboards/dashboard.yml", BindMode.READ_ONLY)
+			.withClasspathResourceMapping("microservices-spring-boot-2-1_rev1.json",
+					"/etc/grafana/provisioning/dashboards/microservices-spring-boot-2-1_rev1.json", BindMode.READ_ONLY)
+			.withClasspathResourceMapping("grafana.ini", "/etc/grafana/grafana.ini", BindMode.READ_ONLY)
+			.withExposedPorts(3000);
+		return container;
+	}
 
-    public static void main(String[] args) {
-        SpringApplication.from(Application::main).with(TestApplication.class).run(args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.from(Application::main).with(TestApplication.class).run(args);
+	}
 
 }
